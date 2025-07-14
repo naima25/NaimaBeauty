c54e3f7ae0dffa15634b1b0783f031df9b7241c5
Naima9991 <150269222+Naima9991@users.noreply.github.com>
2025-07-14 17:16:44 +0400

    Tested Cart Service

---
+++ Services/CartService.cs
@@ -0,0 +1,51 @@
+using NaimaBeauty.Models;
+using NaimaBeauty.Data;
+using NaimaBeauty.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NaimaBeauty.Services
+{
+    public class CartService : ICartRepository
+    {
+        private readonly AppDbContext _context;
+
+        public CartService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Cart>> GetAllAsync()
+        {
+            return await _context.Carts.ToListAsync();
+        }
+
+        public async Task<Cart?> GetByIdAsync(int id)
+        {
+            return await _context.Carts.FindAsync(id);
+        }
+
+        public async Task AddAsync(Cart cart)
+        {
+            _context.Carts.Add(cart);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(int id, Cart cart)
+        {
+            _context.Entry(cart).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var cart = await _context.Carts.FindAsync(id);
+            if (cart != null)
+            {
+                _context.Carts.Remove(cart);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
