9845bde2a9d3885d6a8da69778c8dc50af604e24
Naima9991 <150269222+Naima9991@users.noreply.github.com>
2025-07-16 11:56:38 +0400

    Integration of Repos and Services

---
+++ Migrations/20250716075523_Services.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NaimaBeauty.Migrations
+{
+    /// <inheritdoc />
+    public partial class Services : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Featured",
+                table: "Products");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Featured",
+                table: "Products",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
